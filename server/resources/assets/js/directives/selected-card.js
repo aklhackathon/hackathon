37bce66f5157a5c89095066ee6082b4d21206d7d
popmaker <aboutjax1990@icloud.com>
2015-05-10 18:06:57 +1200

    Refactor some scss for content heights

--- server/resources/assets/js/directives/selected-card.js
+++ server/resources/assets/js/directives/selected-card.js
@@ -3,6 +3,7 @@
 
 angular.module('CircleOfDeath.directives').directive('selectedCard', function() {
     return {
+        replace: true,
         restrict: 'E',
         templateUrl: 'selected-card.html'
     };
