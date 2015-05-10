b47c5ad807e64cc5c95d1caec1bdee7686a3311f
“Jamie <jamsinclairnz@gmail.com>
2015-05-10 16:49:40 +1200

    Add fetch method to get gameplay data in the landing controller

--- server/resources/assets/js/controllers/landing.js
+++ server/resources/assets/js/controllers/landing.js
@@ -1,6 +1,24 @@
 /*globals angular*/
 'use strict';
 
-angular.module('CircleOfDeath.controllers').controller('LandingCtrl', function() {
-	// Controller logic here
+angular.module('CircleOfDeath.controllers').controller('LandingCtrl', function($scope, $http, Card) {
+	var GAMEPLAY_API_ENDPOINT = 'gameplay.json',
+		_initFetchGameplay;
+
+	// Init scope vars
+	$scope.cards = [];
+	$scope.state = 'in-progress';
+
+	_initFetchGameplay = function() {
+		$http.get(GAMEPLAY_API_ENDPOINT)
+			.then(function(response) {
+				angular.forEach(response.data.gameplay.ruleset.rule_matches, function(ruleMatch) {
+					$scope.cards.push(new Card(ruleMatch));
+				});
+
+				$scope.state = 'ready';
+			});
+	};
+
+	_initFetchGameplay();
 });
