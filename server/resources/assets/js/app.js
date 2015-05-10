82c052af0bf54866be02d40f28efb709b013dfa1
“Jamie <jamsinclairnz@gmail.com>
2015-05-10 14:59:48 +1200

    Init initial app file

--- server/resources/assets/js/app.js
+++ server/resources/assets/js/app.js
@@ -1,1 +1,32 @@
+/*globals require*/
+'use strict';
+
+var angular = window.angular || angular;
+
+// Set up angular component modules
+angular.module('CircleOfDeath.controllers', []);
+angular.module('CircleOfDeath.directives', []);
+angular.module('CircleOfDeath.factories', []);
+angular.module('CircleOfDeath.services', []);
+angular.module('CircleOfDeath.routes', []);
+angular.module('templates', []);
+
+// Adding angular modules
+require('./controllers/controllers');
+require('./routes/routes');
+
+// Adding Angular TemplateCache
+require('./templates');
+
+// Finally setup the app module
+angular.module('CircleOfDeath', [
+	'ngRoute',
+	'CircleOfDeath.controllers',
+	'CircleOfDeath.directives',
+	'CircleOfDeath.factories',
+	'CircleOfDeath.services',
+	'CircleOfDeath.routes',
+	'templates'
+]).routes;
+
 
